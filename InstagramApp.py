f5d6cd8294301b4f53b0bfab28939c7f20359a00
unknown <wangarraakoth@gmail.com>
2022-08-07 17:33:04 +0300

    scrappinginstagram

---
+++ InstagramApp.py
@@ -0,0 +1,38 @@
+import instaloader
+
+# Create an instance of Instaloader class
+bot = instaloader.Instaloader()
+
+# Load a profile from an Instagram handle
+profile = instaloader.Profile.from_username(bot.context, 'akoth845')
+
+
+print(type(profile))
+print("Username: ", profile.username)
+print("User ID: ", profile.userid)
+print("Number of Posts: ", profile.mediacount)
+print("Followers: ", profile.followers)
+print("Followees: ", profile.followees)
+print("Bio: ", profile.biography,profile.external_url)
+
+bot.login(user="your username",passwd="your password")
+
+# Interactive login on terminal
+bot.interactive_login("akoth845") 
+# Asks for password in the terminal
+
+followers = [follower.username for follower in profile.get_followers()]
+
+# Retrieve the usernames of all followees
+followees = [followee.username for followee in profile.get_followees()]
+print(followers)
+
+# Load a new profile
+profile = instaloader.Profile.from_username(bot.context, 'wwe')
+
+# Get all posts in a generator object
+posts = profile.get_posts()
+
+# Iterate and download
+for index, post in enumerate(posts, 1):
+    bot.download_post(post, target=f"{profile.username}_{index}")
